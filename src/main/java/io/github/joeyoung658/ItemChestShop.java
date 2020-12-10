a7a8b595a8a396ad4e0c0499c47c8362ff40c396
joeyoung658 <josephyoung658@gmail.com>
2020-12-10 20:17:57 +0000

    First Commits

--- src/main/java/io/github/joeyoung658/ItemChestShop.java
+++ src/main/java/io/github/joeyoung658/ItemChestShop.java
@@ -1,14 +1,24 @@
 package io.github.joeyoung658;
 
+import io.github.joeyoung658.Listeners.chestShopSetUp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ItemChestShop extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        getLogger().info("Itemchest Shop is enabled");
+        registerListeners();
+        getLogger().info("ItemChestShop is enabled.");
     }
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        //Save ChestShops
     }
+
+
+    private void registerListeners(){
+        getServer().getPluginManager().registerEvents(new chestShopSetUp(), this);
+    }
+
+
 }
---
+++ src/main/java/io/github/joeyoung658/Listeners/chestShopSetUp.java
@@ -0,0 +1,25 @@
+package io.github.joeyoung658.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class chestShopSetUp implements Listener {
+
+    @EventHandler
+    public void signChangeEvent(SignChangeEvent event){
+        String[] signText = event.getLines();
+        String lineOne = signText[0];
+        String lineTwo = signText[1];
+        String lineThree = signText[2];
+        String lineFour = signText[3];
+
+
+
+
+
+    }
+}
