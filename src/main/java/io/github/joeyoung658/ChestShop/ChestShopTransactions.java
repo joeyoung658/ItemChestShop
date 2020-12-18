ab5e88757e8a8a1a1ffd384db78665bde2da85da
joeyoung658 <josephyoung658@gmail.com>
2020-12-18 16:08:52 +0000

    Fixed transactions?

--- src/main/java/io/github/joeyoung658/ChestShop/ChestShopTransactions.java
+++ src/main/java/io/github/joeyoung658/ChestShop/ChestShopTransactions.java
@@ -93,6 +93,7 @@ public class ChestShopTransactions {
         return !Arrays.asList(this.player.getInventory().getStorageContents()).contains(null);
     }
 
+
     private boolean inventoryHasItem(@org.jetbrains.annotations.NotNull Inventory inventory, ItemStack itemStack, int qty){
         if (inventory.isEmpty()){
             return false;
@@ -114,52 +115,39 @@ public class ChestShopTransactions {
 
     private void giveInventoryItem(Inventory inventory, ItemStack itemStack, int qty){
         if (!inventory.contains(itemStack.getType())){
-            this.setEmptyIvenSlot(inventory, itemStack);
+            this.setEmptyIvenSlot(inventory, itemStack, qty);
             return;
         }
+
         for (int i = 0; i < inventory.getSize(); i++) {
             ItemStack item = inventory.getItem(i);
-            if (item != null){
+            if (item != null && item.getAmount() != 64){
                 Bukkit.broadcastMessage(ChatColor.AQUA  + inventory.getItem(i).toString());
                 if (item.getType() == itemStack.getType()){
-                    inventory.setMaxStackSize(qty + item.getAmount());
-                    item.setAmount(qty + item.getAmount());
-                    inventory.setItem(i, item);
+                    int itemAmt = qty + item.getAmount();
+                    if (itemAmt > 64) {
+                        item.setAmount(64);
+                        inventory.setItem(i, item);
+                        qty = itemAmt - 64;
+                        setEmptyIvenSlot(inventory,item, qty);
+                    } else {
+                        item.setAmount(itemAmt);
+                        inventory.setItem(i, item);
+                    }
                     return;
                 }
+
             }
+        }
+        if (qty != 0){
+            setEmptyIvenSlot(inventory,itemStack, qty);
+            return;
         }
     }
 
 
-
-    private void giveInventoryItemV1(Inventory inventory, ItemStack itemStack, int qty){
-        if (inventory.contains(itemStack.getType())){
-            for (int i = 0; i < inventory.getSize(); i++) {
-                if (inventory.getItem(i) != null){
-                    if (inventory.getItem(i).getType() == itemStack.getType()) {
-                        ItemStack current = inventory.getItem(i);
-                        if ((current.getAmount() + qty) <= 64) {
-                            current.setAmount(current.getAmount() + qty);
-                            inventory.setItem(i, current);
-                            return;
-                        } else {
-                            qty = (64 - (current.getAmount() + qty));
-                            current.setAmount(64);
-                        }
-                    }
-                }
-            }
-            if (qty != 0){
-                itemStack.setAmount(qty);
-                this.setEmptyIvenSlot(inventory, itemStack);
-            }
-        } else {
-            this.setEmptyIvenSlot(inventory, itemStack);
-        }
-    }
-
-    private void setEmptyIvenSlot(Inventory inventory, ItemStack itemStack){
+    private void setEmptyIvenSlot(Inventory inventory, ItemStack itemStack, int qty){
+        itemStack.setAmount(qty);
         for (int i = 0; i < inventory.getSize(); i++) {
             if(inventory.getItem(i) == null) {
                 inventory.setItem(i, itemStack);
